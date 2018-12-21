6f47b3d454969a287ea4c1eb07c55878ba0042d9
Abel-Huang <497720975@qq.com>
2018-12-21 16:22:21 +0800

    init proj

---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package singleton;
+
+/**
+ * @Author: abel-huang
+ * @Date: 2018-12-21 16:14
+ */
+public class Singleton {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
